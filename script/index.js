1db305f376e2e7e50cbb6044688f08d6bcfdbb46
shams al labib <samslabib19@gmail.com>
2025-09-04 13:50:25 +0600

    added search section

--- script/index.js
+++ script/index.js
@@ -167,3 +167,22 @@ const displayLessons = (data) => {
 
 
 loadLessons();
+
+
+// search section
+
+document.getElementById('btn-search').addEventListener('click', ()=> {
+    removeActive()
+    const input = document.getElementById('input-search');
+    const searchValue = input.value.trim().toLowerCase();
+    console.log(searchValue)
+
+    fetch('https://openapi.programming-hero.com/api/words/all')
+    .then(res => res.json())
+    .then(data => {
+        const allWords = data.data;
+        console.log(allWords)
+        const filterWord = allWords.filter(word => word.word.toLowerCase().includes(searchValue));
+        displayLevelWord(filterWord)
+    })
+})
